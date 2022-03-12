c378318f713e7f360e921f2758b55b52b9ae4a08
ryoichikawa1202 <1202.ryo1997@gmail.com>
2022-03-12 05:42:27 +0000

    [Add]favorite

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ApplicationRecord
+
   belongs_to :user
   has_many :article_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
