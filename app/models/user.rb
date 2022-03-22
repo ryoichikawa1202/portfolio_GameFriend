d555a7fa85c71d9c9c0e2f1928641b4f9ca06fa1
ryoichikawa1202 <1202.ryo1997@gmail.com>
2022-03-22 06:20:00 +0000

    [Add]bootstrap/layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+  validates :name, presence: true
+
 
   def get_profile_image(width, height)
     unless profile_image.attached?
